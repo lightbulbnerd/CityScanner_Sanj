c16d2bb866c1fb574136f3f8bb5ba84fdd7dc99d
Sanjana P <31711564+lightbulbnerd@users.noreply.github.com>
2021-04-13 13:47:48 -0400

    Update hotspot_preprocess.py

--- hotspot_preprocess.py
+++ hotspot_preprocess.py
@@ -1,4 +1,4 @@
-#hotspot background processing
+%%capture
 #libraries
 import csv
 import pytz
@@ -6,6 +6,8 @@ import time
 %pip install pandas
 import pandas as pd
 from datetime import datetime
+import os
+os.environ['R_HOME'] = '/path/to/R'
 
 #analysis of dataset collected by two low-cost OPC-N2s on two trash-trucks in NYC
 
@@ -21,77 +23,41 @@ print("aq read")
 times = data.loc[:,['time']]
 df = pd.DataFrame(times)
 df['time'] = pd.to_datetime(df['time'], unit='s')
-print(df)
-print(df.dtypes)
+#print(df)
+#print(df.dtypes)
 
-#remove sfr
+
+#sfr
+sfr_in = data.loc[:,['sfr']]
+sfr_df = pd.DataFrame(sfr_in)
+#r code (below) says remove - investigate paste0 
 # /// aq$sfr<-as.numeric(as.character(paste0(aq$sfr)))
 
 #add up particles from bins
 #Number concentration of particles between 0.38 and 1 micrometer
 n1_out = data['n1_out'] = data.loc[:,['bin0','bin1', 'bin2']].sum(axis=1)
-print("N1")
-print(n1_out)
+#print("N1")
+#print(n1_out)
 
 #Number concentration of particles between 1 and 3 micrometer
 n3_out = data['n3_out'] = data.loc[:,['bin3','bin4', 'bin5', 'bin6']].sum(axis=1)
-print("N3")
-print(n3_out)
+#print("N3")
+#print(n3_out)
 
 #Number concentration of particles between 1 and 10 micrometer
 n10_out = data['n10_out'] = data.loc[:,['bin7','bin8', 'bin9', 'bin10', 'bin11']].sum(axis=1)
-print("N10")
-print(n10_out)
+#print("N10")
+#print(n10_out)
 
 #something about NAs
 #aq<-aq[!is.na(aq$n1),]
 
-#divide by sfr
+#divide by sfr 
 '''
-n1_out1 = (n1/out)/sfr
-n3_out1 = (n3/out)/sfr
-n10_out1 = (n10/out)/sfr
-
-in essence this is it - need to define sfr and perform division through column though
-'''
-
-
-# this whole thing
-'''
-aq$lat<-as.numeric(aq$lat)
-aq$long<-as.numeric(as.character(paste(aq$long)))
-aq<-subset(aq,aq$lat!=0)
-aq<-subset(aq, !is.na(aq$pm25))
-aq$bin13<-as.numeric(as.character(paste0(aq$bin13)))
-aq$bin12<-as.numeric(as.character(paste0(aq$bin12)))
-aq$bin14<-as.numeric(as.character(paste0(aq$bin14)))
-aq$bin15<-as.numeric(as.character(paste0(aq$bin15)))
-aq$bin16<-as.numeric(as.character(paste0(aq$bin16)))
-aq$bin17<-as.numeric(as.character(paste0(aq$bin17)))
-aq$bin13<-as.numeric(as.character(paste0(aq$bin13)))
-aq$bin19<-as.numeric(as.character(paste0(aq$bin19)))
-aq$bin20<-as.numeric(as.character(paste0(aq$bin20)))
-aq$bin21<-as.numeric(as.character(paste0(aq$bin21)))
-aq$bin22<-as.numeric(as.character(paste0(aq$bin22)))
-aq$bin23<-as.numeric(as.character(paste0(aq$bin23)))
-'''
-
-from rpy2.robjects.packages import importr
-    openair = importr('openair')
-
-
-#and then this whole thing
-
-'''
-#Creating date
-aq$date<-substring(aq$time,1,10)
-aq$date<-as.POSIXct(aq$date, format="%Y-%m-%d")
-aq$date<-format(aq$date, "%Y-%m-%d")
-aq$date<-as.Date(aq$date," %Y-%m-%d")
-
-aq<-aq[!is.na(aq$n3),]
-aq$x<- aq$join_x
-aq$y<-aq$join_y
-aq$pmw10b_NYCCAS<-aq$join_pmw10b
-aq<-aq[ , !grepl( "join" , names(aq) ) ]
+data['n1_out1'] = data[n1_out]/data[sfr_df]
+print(n10_out)
+n3_out1 = (n3_out)/sfr_df
+print(n10_out)
+n10_out1 = (n10_out)/sfr_df
+print(n10_out)
 '''
