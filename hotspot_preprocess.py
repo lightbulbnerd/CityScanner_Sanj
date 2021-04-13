c6a5243eb398085da4663d0487e01cb73598c3e7
Sanjana P <31711564+lightbulbnerd@users.noreply.github.com>
2021-04-13 09:00:01 -0400

    Updated code - still in process

--- hotspot_preprocess.py
+++ hotspot_preprocess.py
@@ -1,43 +1,97 @@
 #hotspot background processing
-
+#libraries
 import csv
+import pytz
+import time
+%pip install pandas
 import pandas as pd
 from datetime import datetime
 
 #analysis of dataset collected by two low-cost OPC-N2s on two trash-trucks in NYC
-date_default_timezone_set("America/NewYork");
-#Reading in the data
 
-#read in csv
-with open("/Users/sanjanapaul/Downloads/AQ_orgfid.csv") as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        else:
-            print('Line read!')
-            line_count += 1
-    print(f'Processed {line_count} lines.')
-#aq<-read.csv("nyc_aq_joined.csv")
+#set timezone
+tz = pytz.timezone('US/Eastern')
+
+#read in OPC data
+aq = "AQ_orgfid.csv"
+data = pd.read_csv(aq, engine='python')
 print("aq read")
 
-# just ran chunk 3 in the r code, which is what this is. compare actual and
-# desired results and adjust as necessary
+#format time
+times = data.loc[:,['time']]
+df = pd.DataFrame(times)
+df['time'] = pd.to_datetime(df['time'], unit='s')
+print(df)
+print(df.dtypes)
 
-# cleaning data below -
-# subset=None:every column is used to determine if two rows are different;
-# to change that specify columns as an array
-# inplace=True:the data structure is changed and duplicate rows are gone
+#remove sfr
+# /// aq$sfr<-as.numeric(as.character(paste0(aq$sfr)))
+
+#add up particles from bins
+#Number concentration of particles between 0.38 and 1 micrometer
+n1_out = data['n1_out'] = data.loc[:,['bin0','bin1', 'bin2']].sum(axis=1)
+print("N1")
+print(n1_out)
+
+#Number concentration of particles between 1 and 3 micrometer
+n3_out = data['n3_out'] = data.loc[:,['bin3','bin4', 'bin5', 'bin6']].sum(axis=1)
+print("N3")
+print(n3_out)
+
+#Number concentration of particles between 1 and 10 micrometer
+n10_out = data['n10_out'] = data.loc[:,['bin7','bin8', 'bin9', 'bin10', 'bin11']].sum(axis=1)
+print("N10")
+print(n10_out)
+
+#something about NAs
+#aq<-aq[!is.na(aq$n1),]
+
+#divide by sfr
 '''
-aq_in = "AQ_orgfid.csv"
-aq = "AQ_orgfid_clean.csv"
-df = pd.read_csv(file_in, sep="\t or ,")
-df.drop_duplicates(subset=None, inplace=True)
-df.to_csv(file_name_output, index=False)
+n1_out1 = (n1/out)/sfr
+n3_out1 = (n3/out)/sfr
+n10_out1 = (n10/out)/sfr
 
-#Formatting string from CSV to time
-pd.to_datetime(aq$time, origin="1970-01-01 00:00")
-pd.to_datetime(aq$date, format'"%Y-%m-%d")
+in essence this is it - need to define sfr and perform division through column though
+'''
+
+
+# this whole thing
+'''
+aq$lat<-as.numeric(aq$lat)
+aq$long<-as.numeric(as.character(paste(aq$long)))
+aq<-subset(aq,aq$lat!=0)
+aq<-subset(aq, !is.na(aq$pm25))
+aq$bin13<-as.numeric(as.character(paste0(aq$bin13)))
+aq$bin12<-as.numeric(as.character(paste0(aq$bin12)))
+aq$bin14<-as.numeric(as.character(paste0(aq$bin14)))
+aq$bin15<-as.numeric(as.character(paste0(aq$bin15)))
+aq$bin16<-as.numeric(as.character(paste0(aq$bin16)))
+aq$bin17<-as.numeric(as.character(paste0(aq$bin17)))
+aq$bin13<-as.numeric(as.character(paste0(aq$bin13)))
+aq$bin19<-as.numeric(as.character(paste0(aq$bin19)))
+aq$bin20<-as.numeric(as.character(paste0(aq$bin20)))
+aq$bin21<-as.numeric(as.character(paste0(aq$bin21)))
+aq$bin22<-as.numeric(as.character(paste0(aq$bin22)))
+aq$bin23<-as.numeric(as.character(paste0(aq$bin23)))
+'''
+
+from rpy2.robjects.packages import importr
+    openair = importr('openair')
+
+
+#and then this whole thing
+
+'''
+#Creating date
+aq$date<-substring(aq$time,1,10)
+aq$date<-as.POSIXct(aq$date, format="%Y-%m-%d")
+aq$date<-format(aq$date, "%Y-%m-%d")
+aq$date<-as.Date(aq$date," %Y-%m-%d")
+
+aq<-aq[!is.na(aq$n3),]
+aq$x<- aq$join_x
+aq$y<-aq$join_y
+aq$pmw10b_NYCCAS<-aq$join_pmw10b
+aq<-aq[ , !grepl( "join" , names(aq) ) ]
 '''
